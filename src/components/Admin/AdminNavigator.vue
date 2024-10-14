39e746d8ed50c9a458b24b94f28b75cfe12ddb5f
ntrong128 <lenhattrong128@gmail.com>
2024-10-14 22:17:02 +0700

    split user and admin route

---
+++ src/components/Admin/AdminNavigator.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="border-b">
+    <div class="container flex justify-between items-center py-6">
+      <h1 class="text-3xl font-bold">Library Dashboard</h1>
+      <nav class="flex">
+        <RouterLink v-for="(item, index) in navItems" :key="index" :to="item.path">
+          <Button variant="ghost" class="mx-2">{{ item.label }}</Button>
+        </RouterLink>
+        <ThemeToggle />
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { Button } from '@/components/ui/button'
+import ThemeToggle from '@/components/ThemeToggle.vue'
+
+const navItems = [
+  { path: '/dashboard', label: 'Dashboard' },
+  { path: '/dashboard/books', label: 'Book Management' },
+  { path: '/dashboard/applications', label: 'Borrow application' }
+]
+</script>
---
+++ src/views/user/UserView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore';
+import { RouterView } from 'vue-router'
+import landingHeader from '@/components/header/landingHeader.vue'
+import loggedInHeader from '@/components/header/loggedInHeader.vue'
+const authStore = useAuthStore()
+
+</script>
+
+<template>
+    <loggedInHeader v-if="authStore.isAuthenticated" />
+    <landingHeader v-else />
+    <RouterView></RouterView>
+</template>
