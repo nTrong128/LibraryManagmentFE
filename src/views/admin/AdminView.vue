40f98758660b15f75268f10073cb04eb7cd90888
ntrong128 <lenhattrong128@gmail.com>
2024-11-11 20:47:30 +0700

    feat: landing page

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import Header from '@/components/Admin/AdminNavigator.vue';
+import AdminSidebar from '@/components/Admin/Sidebar.vue'
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Separator } from '@/components/ui/separator';
 </script>
 
 <template>
-  <Header></Header>
-  <RouterView />
+  <div class="flex h-screen">
+    <AdminSidebar />
+    <ScrollArea class="flex-shrink w-full">
+
+      <RouterView />
+
+    </ScrollArea>
+  </div>
 </template>
