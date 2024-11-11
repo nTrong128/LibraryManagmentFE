72659294aacdcb60a26d3328a75ad969af4de0f1
ntrong128 <lenhattrong128@gmail.com>
2024-11-11 20:47:19 +0700

    feat: oops, forgot commit

---
+++ src/views/admin/EmployeeManagement.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mx-auto py-6">
+        <div class="flex justify-between">
+            <h1 class="text-3xl font-bold mb-6">Quản lý nhân viên</h1>
+            <AddEmployee></AddEmployee>
+        </div>
+        <EmployeeTable />
+    </div>
+</template>
+
+<script setup lang="ts">
+import EmployeeTable from '@/components/Admin/EmployeeManagement/EmployeeTable.vue';
+import AddEmployee from '@/components/Admin/EmployeeManagement/AddEmployee.vue';
+
+
+</script>
---
+++ src/views/admin/PublisherManagement.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container mx-auto py-6">
+        <div class="flex justify-between">
+            <h1 class="text-3xl font-bold mb-6">Quản lý nhà xuất bản</h1>
+            <AddPublisher></AddPublisher>
+        </div>
+        <PublisherTable />
+    </div>
+</template>
+
+<script setup lang="ts">
+import PublisherTable from '@/components/Admin/Publisher/PublisherTable.vue';
+import AddPublisher from '@/components/Admin/Publisher/AddPublisher.vue';
+
+
+</script>
